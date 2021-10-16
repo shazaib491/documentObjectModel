667f602e5e4c4314a646457f72a14e10ab1b2a24
shazaib491 <shazaibrehman127@gmail.com>
2021-10-16 21:44:29 +0530

    16OCt

--- index3.js
+++ index3.js
@@ -6,8 +6,8 @@
 //     heading.style="color:pink;background-color:red"
 // }
 
-let no=20;
-let no2=30;
+let no = 20;
+let no2 = 30;
 // let no3="first no is \n"+no+"Second no is "+no2+" \t"+" sum of this no is "+parseInt(no+no2)
 // template literal
 // let a=`hello wolrd`
@@ -15,30 +15,72 @@ let no2=30;
 // let no4=`first no is ${no} \n second no is ${no2} \n sum of this no is ${no+no2}`
 // console.log(no3)
 // console.log(no4)
-const heading=document.querySelector(".head");
-const inputI=document.querySelector("#demo")
-const inputII=document.querySelector("#demo1")
-heading.style=`color:red`
+const heading = document.querySelector(".head");
+const inputI = document.querySelector("#demo")
+const inputII = document.querySelector("#demo1")
+heading.style = `color:red`
 
-function clickme(){
-heading.style="background-image:url(https://picsum.photos/200/300)"
+function clickme() {
+    heading.style = "background-image:url(https://picsum.photos/200/300)"
 }
 
-const user={
-    name:"",
-    email:""
+const user = {
+    name: "",
+    email: ""
 }
-function changer(){
-    user.name=inputI.value
-    
+function changer() {
+    user.name = inputI.value
+
 }
 
-function changer2(){
-    user.email=inputII.value
+function changer2() {
+    user.email = inputII.value
 }
 
-function submit(){
+function submit() {
     console.log(user)
 }
 
 
+function displaySum(answer) {
+    console.log(answer)
+}
+
+function sum(sums) {
+    // let c=prompt("Enter number");
+    // sums(c)
+    console.log("completed")
+}
+
+sum(displaySum)
+
+let click = document.querySelector("#clicked");
+let name, emai, password;
+let arr = [];
+click.addEventListener('submit', (e) => {
+    e.preventDefault();
+    name = e.target.name.value;
+    email = e.target.email.value;
+    password = e.target.password.value;
+    arr.push({ name, email, password })
+    click.reset();
+    console.log(arr)
+    myfunc();
+})
+
+function myfunc() {
+    document.getElementById("root").innerText = `
+        ${arr.map((e) => {
+        return `
+            <li>${e.name}</li>
+            <li>${e.email}</li>
+            <li>${e.password}</li>
+            `
+    })}
+    `
+}
+// const saved = () => {
+//     event.preventDefault();
+//     console.log(event)
+//     console.log(event.target[0].value)
+// }
